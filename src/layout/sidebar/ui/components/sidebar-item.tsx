9a5012e80225e35d1128b88ebeb5940224e59568
mintjulep21 <zharylkassyn001@gmail.com>
2024-05-30 10:41:01 +0500

    sidebar fix

--- src/layout/sidebar/ui/components/sidebar-item.tsx
+++ src/layout/sidebar/ui/components/sidebar-item.tsx
@@ -24,7 +24,7 @@ export const SidebarItem = ({ icon, path, title, tab }: IProps) => {
         isActive ? "text-gray-600" : "text-gray-900"
       } ${isDesktop ? "h-16" : "h-12"}`}
     >
-      <div className="flex items-center w-full justify-center">
+      <div className="flex items-center gap-2 w-full justify-start">
         {icon}
         {isDesktop && <p>{title}</p>}
       </div>
